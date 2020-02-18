6fb4db57b3738a9c8d4c4a7050247ee079a3076a
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-02-18 14:31:57 +0530

    created main definition class methods

--- src/definition/MyQueue.java
+++ src/definition/MyQueue.java
@@ -1,4 +1,87 @@
 package definition;
 
-public class MyQueue {
+import adt.QueueADT;
+
+public class MyQueue<E> implements QueueADT<E> {
+
+    private Node<E> front = null;
+    private Node<E> rear = null;
+    private int size = 0;
+
+    private boolean addFirst(E item) {
+        return true;
+    }
+
+    private boolean addAfter(Node<E> node, E item) {
+        return true;
+    }
+
+    private Node<E> getNode(int index) {
+        return null;
+    }
+
+    public boolean add(int index, E item) {
+        return true;
+    }
+
+    @Override
+    public boolean add(E item) {
+        return false;
+    }
+
+    private E removeFirst() {
+        return null;
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public E poll() {
+        return null;
+    }
+
+    @Override
+    public E element() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("[");
+        sb.append("]");
+        return sb.toString();
+    }
+
+    private static class Node<E> {
+        private E data;
+        private Node<E> previous;
+        private Node<E> next;
+
+        private Node(E data, Node<E> previous, Node<E> next) {
+            this.data = data;
+            this.previous = previous;
+            this.next = next;
+        }
+
+        private E getData() {
+            return data;
+        }
+
+        private Node<E> getPrevious() {
+            return previous;
+        }
+
+        private Node<E> getNext() {
+            return next;
+        }
+    }
+
 }
